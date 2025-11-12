2e5d0dfbf2413d606d1a5349862de77b5d2383a0
marcelof0nseca <maf@cesar.school>
2025-11-11 21:52:53 -0300

    Atualização da estrutura do sistema e inicio do codigo (main)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import Armazenamento_de_Dados
-import cadastro_de_cuidados_e_atividades
-import Contagem_Regressiva_e_Alertas
-import crud
-import Sugestões_Personalizadas
-import adocao_recomendas
+# import Armazenamento_de_Dados
+# import cadastro_de_cuidados_e_atividades
+# import Contagem_Regressiva_e_Alertas
+# import crud
+# import Sugestões_Personalizadas
+# import adocao_recomendas
+
+print('BEM VINDO AO SISTEMA DE ADOCAO DE ANIMAIS')
+while True:
+    pergunta = str(input('1. CRUD\n2. CADASTRO DE CUIDADO E TAREFAS\n3. CONTAGEM REGRESSIVA E ALERTAS\n4. SAIR\nDigite a opção desejada: ')).strip()
+    if pergunta == '1':
+        
