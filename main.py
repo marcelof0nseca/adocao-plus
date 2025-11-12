b894f0847a3c08a0e5814652e5faea4ebba57e42
marcelof0nseca <maf@cesar.school>
2025-11-11 21:55:05 -0300

    Atualização da estrutura do sistema e inicio do codigo (main)

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ print('BEM VINDO AO SISTEMA DE ADOCAO DE ANIMAIS')
 while True:
     pergunta = str(input('1. CRUD\n2. CADASTRO DE CUIDADO E TAREFAS\n3. CONTAGEM REGRESSIVA E ALERTAS\n4. SAIR\nDigite a opção desejada: ')).strip()
     if pergunta == '1':
-        
+        print('em andamento')
