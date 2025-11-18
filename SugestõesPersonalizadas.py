120ae7af529788e0557710ebcd91874ab102a1e9
Mafra <neto.dida@gmail.com>
2025-11-18 09:14:44 -0300

    Criando estrutura basica da sujestoes personalizadas

---
+++ SugestõesPersonalizadas.py
@@ -0,0 +1,10 @@
+def adocaointeligente():
+def cuidadosespeciais():
+def
+def
+while True:
+    try:
+        escolha=int(input('1 - adoção inteligente\n2 - cuidados especiais\n3- compatibilidade com outros animais\n4 - atividades de socialização '))
+    except:
+        print('Digite um numero inteiro')
+    if escolha == 1:
